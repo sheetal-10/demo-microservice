50af96f06bac0eb63fd75e3bc4622949905f4619
Sheetal Panda <sheetalpanda@Sheetals-MacBook-Pro.local>
2022-06-01 23:01:33 +0530

    [Sheetal] Add. initial project setup

---
+++ src/test/java/com/gdo/bootcamp/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdo.bootcamp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
